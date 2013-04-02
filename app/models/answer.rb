707c183c5957bfe22dd694e5689c3d1fa1a0a821
Brian&Ian <david+luke@appacademy.io>
2013-04-02 14:08:00 -0700

    validate uniqueness of respondent per question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,16 @@
 class Answer < ActiveRecord::Base
   belongs_to :choice
-  belongs_to :respondent, :class => User
+  belongs_to :respondent, :class_name => "User"
+
+  validate :validate_unique_answer
 
   attr_accessible :choice_id, :respondent_id
+
+  def validate_unique_answer
+    self.choice
+        .question
+        .answers
+        .where("respondent_id = ?", self.respondent_id)
+        .count == 0
+  end
 end
